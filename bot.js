6b5deae6fabbe9e4376ccd4211c69ffbdb116aa9
Scott Moak <scott.moak@gmail.com>
2011-08-09 16:50:08 -0700

    Initial commit. Using node-mpd from https://github.com/robinduckett/node-mpd

---
+++ bot.js
@@ -0,0 +1,62 @@
+var irc = require('irc'),
+    options = require("nomnom").opts({
+      host: {
+        string: "-H HOST, --host=HOST",
+        default: "irc.freenode.net",
+        help: "What IRC network to connect to. (Default: irc.freenode.net)"
+      },
+      nick: {
+        string: "-n NICK, --nick=NICK",
+        default: "MBot",
+        help: "IRC nick to use. (Default: MBot)"
+      },
+      channels: {
+        string: "-c CHANNELS, --channels=CHANNELS",
+        default: "",
+        help: "IRC channels to join (comma-separated, no '#')."
+      },
+      mpd_port: {
+        string: "-mp PORT --mpd_port==PORT",
+        default: 6600,
+        help: "MPD Port to connect to. (Default: 6600)"
+      },
+      mpd_host: {
+        string: "-mh HOST --mpd_host==HOST",
+        default: "localhost",
+        help: "MPD Host to connect to. (Default: localhost)"
+     }
+    }).parseArgs();
+
+var MPD = require('./node-mpd/lib/index.js');
+var mpd = new MPD(options.mpd_host, options.mpd_port);
+mpd.debug = true;
+
+var CHANNELS = options.channels.split(',');
+CHANNELS.forEach(function(channel, i) {
+  CHANNELS[i] = '#' + channel.trim();
+});
+
+var client = new irc.Client(options.host, options.nick, {
+  channels: CHANNELS
+});
+
+client.addListener('message', function (from, to, message) {
+  var target, isChannel = false;
+  if (to.indexOf("#") == 0) {
+    target = to;
+    isChannel = true;
+  } else {
+    target = from;
+  }
+
+  if (isChannel) {
+    if (message.indexOf(options.nick) == 0) {
+      if (message.match(/whats playing/)) {
+        mpd.send('currentsong', function(cs) {
+          client.say(target, cs.Artist + " - " + cs.Title);
+          console.log("Got current song info: " + cs);
+        });
+      }
+    }
+  }
+});
